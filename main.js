885e523d15efdba463725da83a64302cc8052e09
nos-joel <nos_joel@hotmail.com>
2016-05-30 11:11:51 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    
+$("#scan").click(function(){
+   $("#scancontent").show();
+    $("#logobottom").hide();
+    $("#splash").hide();
+});    
+
+$("#selectFile").click(function(){
+   $("#photosubmit").show();
+});  
+    
+ 
+ $("#photosubmit").click(function(){
+   $("#scancontent").hide();
+     $("#photosubmit").hide();
+     $("#demoflier").show();
+}); 
+ 
+    
+});    
