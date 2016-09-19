a37b2a3e6a05f7edaedaaf55eadf70c926eb7ea6
nos-joel <nos_joel@hotmail.com>
2016-09-19 15:15:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
 $(document).ready(function(){
     
-$("#scan").click(function(){
-   $("#scancontent").show();
-    $("#logobottom").hide();
-    $("#splash").hide();
-});    
+	$("#scan").click(function(){
+		$("#scancontent").show();
+		$("#logobottom").hide();
+		$("#splash").hide();
+	});    
 
-$("#selectFile").click(function(){
-   $("#photosubmit").show();
-});  
+	$("#selectFile").click(function(){
+		$("#photosubmit").show();
+	});  
     
- 
- $("#photosubmit").click(function(){
-   $("#scancontent").hide();
-     $("#photosubmit").hide();
-     $("#demoflier").show();
-}); 
- 
+	$("#photosubmit").click(function(){
+		$("#scancontent").hide();
+		$("#photosubmit").hide();
+		$("#demoflier").show();
+	}); 
     
+	$("#categ").click(function(){
+		$("#scancontent").hide();
+		$("#photosubmit").hide();
+		$("#splash").hide();
+		$("#categ")".show();
+		
+	});
+	
 });    
